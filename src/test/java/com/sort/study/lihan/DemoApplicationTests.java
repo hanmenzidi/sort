0cc27f594669613994cf1b49e3fe92f675d870a8
lihan <lihan997@163.com>
2021-09-23 15:55:00 +0800

    排序项目初始化

---
+++ src/test/java/com/sort/study/lihan/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sort.study.lihan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
